7317fa7496c299c05ec79c544ff0c64f0b12495a
hexinze <hexinze@ajmide.com>
2017-10-09 15:41:45 +0800

    add get

--- app/src/main/java/com/example/tse/listviewdemo/MainActivity.java
+++ app/src/main/java/com/example/tse/listviewdemo/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.tse.listviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+
+                .build();
+
+        GitHubService service = retrofit.create(GitHubService.class);
+        Call<List<Repo>> repos =  service.listRepos("HSin-Tse");
+
+        repos.enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                Log.d("tse","repos.size():"+response.body().size());
+                Log.d("tse","repos.size():"+response.body().get(0).getName());
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+
+            }
+        });
     }
 }
