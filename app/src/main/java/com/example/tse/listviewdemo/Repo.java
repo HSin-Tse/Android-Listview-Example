8ca81058d39e7b63c722683a5b9b98aae3ccac87
hexinze <hexinze@ajmide.com>
2017-10-09 15:41:34 +0800

    add bean

---
+++ app/src/main/java/com/example/tse/listviewdemo/Repo.java
@@ -0,0 +1,19 @@
+package com.example.tse.listviewdemo;
+
+/**
+ * Created by tse on 2017/10/9.
+ */
+public class Repo {
+
+
+
+    String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
